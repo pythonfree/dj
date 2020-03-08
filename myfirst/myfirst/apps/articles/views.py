34c59b6f7238f8efd36922ab9d052b1d52ba03cd
pseex <79115839983@ya.ru>
2020-03-08 22:43:46 +0300

    end ))

--- myfirst/myfirst/apps/articles/views.py
+++ myfirst/myfirst/apps/articles/views.py
@@ -1,7 +1,44 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render
+
+from django.urls import reverse
+
+from .models import Article
+
 
 def index(request):
-    return HttpResponse("Привет, мир!")
+    latest_articles_list = Article.objects.order_by('-pub_date')[:5]
+    return render(request, 'articles/list.html', {'latest_articles_list': latest_articles_list})
 
+
+def detail(request, article_id):
+    try:
+        a = Article.objects.get(id=article_id)
+    except:
+        raise Http404("Статья не найдена!")
+
+    latest_comment_list = a.comment_set.order_by('-id')[:10]
+
+    return render(request, 'articles/detail.html', {'article': a, 'latest_comment_list': latest_comment_list})
+
+
+def leave_comment(request, article_id):
+    try:
+        a = Article.objects.get(id=article_id)
+    except:
+        raise Http404("Статья не найдена!")
+
+    a.comment_set.create(author_name=request.POST['name'], comment_text=request.POST['text'])
+
+    return HttpResponseRedirect(reverse('articles:detail', args=(a.id,)))
+
+
+from django.http import HttpResponse
+
+
+#
+# def index(request):
+#     return HttpResponse("Привет, мир!")
+#
 def test(request):
     return HttpResponse("ТЕСТОВАЯ СТРАНИЦА")

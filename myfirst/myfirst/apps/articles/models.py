a9a47b726d715b34d4821e50effa5f84b2abc7bf
pseex <79115839983@ya.ru>
2020-03-08 13:11:41 +0300

    руссификация 1

--- myfirst/myfirst/apps/articles/models.py
+++ myfirst/myfirst/apps/articles/models.py
@@ -1,4 +1,7 @@
+import datetime
 from django.db import models
+
+from django.utils import timezone
 
 
 class Article(models.Model):
@@ -9,11 +12,21 @@ class Article(models.Model):
     def __str__(self):
         return self.article_title
 
+    def was_published_recently(self):
+        return self.pub_date >= (timezone.now() - datetime.timedelta(days=7))
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
 
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     author_name = models.CharField('имя автора', max_length=50)
     comment_text = models.CharField('текст комментария', max_length=200)
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
 
     def __str__(self):
         return self.author_name

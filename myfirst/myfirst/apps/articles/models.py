56de20d11ecc29c152cbe476e0c4e1ecf127ca65
pseex <79115839983@ya.ru>
2020-03-08 10:46:07 +0300

    initial migration

--- myfirst/myfirst/apps/articles/models.py
+++ myfirst/myfirst/apps/articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Artilce(models.Model):
+    article_title = models.CharField('название статьи', max_length=200)
+    artilce_text = models.TextField('текст статьи')
+    pub_date = models.DateTimeField('дата публикации')
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Artilce, on_delete=models.CASCADE)
+    author_name = models.CharField('имя автора', max_length=50)
+    comment_text = models.CharField('текст комментария', max_length=200)

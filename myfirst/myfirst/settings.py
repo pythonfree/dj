38ce384100471b1b1b8c2bc15b72867c7766735f
pseex <79115839983@ya.ru>
2020-03-08 13:48:32 +0300

    install grappelli admin

--- myfirst/myfirst/settings.py
+++ myfirst/myfirst/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
     'articles.apps.ArticlesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
@@ -118,8 +120,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')

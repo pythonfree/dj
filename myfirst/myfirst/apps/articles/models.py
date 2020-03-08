f5b8019625ceef91b1d4033a999bd5ce23e5d0b1
pseex <79115839983@ya.ru>
2020-03-08 11:18:41 +0300

    def __str__(self)

--- myfirst/myfirst/apps/articles/models.py
+++ myfirst/myfirst/apps/articles/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
 
-class Artilce(models.Model):
+class Article(models.Model):
     article_title = models.CharField('название статьи', max_length=200)
-    artilce_text = models.TextField('текст статьи')
+    article_text = models.TextField('текст статьи')
     pub_date = models.DateTimeField('дата публикации')
+
+    def __str__(self):
+        return self.article_title
 
 
 class Comment(models.Model):
-    article = models.ForeignKey(Artilce, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
     author_name = models.CharField('имя автора', max_length=50)
     comment_text = models.CharField('текст комментария', max_length=200)
+
+    def __str__(self):
+        return self.author_name
